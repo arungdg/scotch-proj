55fc1bd4c80a1b56c3c98685cddf53d174a72a58
Arun Gadag <arun.gadag@awnics.com>
2017-08-31 11:52:46 +0530

    Updated UI

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -15,7 +15,6 @@ import { HomeComponent } from './homePage/home.component';
 import { PhotosComponent } from './photos/photos.component';
 import { VideoComponent } from './videos/video.component';
 import { GridComponent } from './gridView/grid.component';
-import { ListComponent } from './listView/list.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { SettingsComponent } from './settings/settings.component';
@@ -29,7 +28,6 @@ const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'photos', component: PhotosComponent },
   { path: 'videos', component: VideoComponent },
-  { path: 'listView', component: ListComponent },
   { path: 'gridView', component: GridComponent },
   { path: 'settings', component: SettingsComponent },
   { path: 'profile', component: ProfileComponent },
@@ -55,7 +53,6 @@ const appRoutes: Routes = [
     HomeComponent,
     PhotosComponent,
     VideoComponent,
-    ListComponent,
     GridComponent,
     NavbarComponent,
     FooterComponent,
